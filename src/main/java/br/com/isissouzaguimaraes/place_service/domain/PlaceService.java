b5477509f9624547326fca03c95a5b64434d6458
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2024-12-23 18:03:26 -0300

    melhoria para slug automatico

--- src/main/java/br/com/isissouzaguimaraes/place_service/domain/PlaceService.java
+++ src/main/java/br/com/isissouzaguimaraes/place_service/domain/PlaceService.java
@@ -1,18 +1,23 @@
 package br.com.isissouzaguimaraes.place_service.domain;
 
+import com.github.slugify.Slugify;
+
 import br.com.isissouzaguimaraes.place_service.api.PlaceRequest;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest) {
         var place = new Place(null, placeRequest.name(),
-                placeRequest.slug(), placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
+                slg.slugify(placeRequest.name()), placeRequest.state(), placeRequest.createdAt(),
+                placeRequest.updatedAt());
         return placeRepository.save(place);
     }
 }
