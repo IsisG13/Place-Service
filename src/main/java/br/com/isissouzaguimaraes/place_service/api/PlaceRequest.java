1efeb09f6db4845c7f7ee6423683c9c946ed16ce
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2024-12-23 18:13:27 -0300

    teste success

--- src/main/java/br/com/isissouzaguimaraes/place_service/api/PlaceRequest.java
+++ src/main/java/br/com/isissouzaguimaraes/place_service/api/PlaceRequest.java
@@ -1,8 +1,6 @@
 package br.com.isissouzaguimaraes.place_service.api;
 
-import java.time.LocalDateTime;
-
 public record PlaceRequest(
-        String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+                String name, String state) {
 
 }
--- src/main/java/br/com/isissouzaguimaraes/place_service/domain/PlaceService.java
+++ src/main/java/br/com/isissouzaguimaraes/place_service/domain/PlaceService.java
@@ -16,8 +16,8 @@ public class PlaceService {
 
     public Mono<Place> create(PlaceRequest placeRequest) {
         var place = new Place(null, placeRequest.name(),
-                slg.slugify(placeRequest.name()), placeRequest.state(), placeRequest.createdAt(),
-                placeRequest.updatedAt());
+                slg.slugify(placeRequest.name()), placeRequest.state(), null,
+                null);
         return placeRepository.save(place);
     }
 }
